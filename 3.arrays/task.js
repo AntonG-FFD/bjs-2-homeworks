e65373f10bc7586d15057ff2c4bb8a0335608bcd
AntonG <akellaww@yandex.ru>
2024-09-07 23:30:36 +0500

    домашка по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+    if ((arr1.length === arr2.length) && (arr1.every((element, index) => element === arr2[index]))) {
+        console.log("массивы одинаковы");
+        return true;
+    } else {
+        console.log("массивы разные");
+        return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter((user) => user.gender === gender).map((user) => user = user.age).reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length-1) {
+        return acc / arr.length
+    } return acc;
+  },0); 
+  return result;
 }
+
+
+
